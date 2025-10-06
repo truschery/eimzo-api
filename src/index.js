38b3b46376064d07cd3ad0f22c8c97f72963006d
truschery <truschery.work@gmail.com>
2025-10-06 15:41:50 +0300

    Добавил экспорт плагина

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import Client from "./Client/Client"
 import EimzoClient from "./Client/EimzoClient"
 
+import PfxPlugin from './Plugins/PfxPlugin'
+
 export {
     Client,
     EimzoClient
+}
+
+// Plugins
+export {
+    PfxPlugin
 }
 
 export default EimzoClient
