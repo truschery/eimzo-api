84a424eee9792bf38fd03790f9f281e18d1c85a2
truschery <truschery.work@gmail.com>
2025-10-06 16:55:29 +0300

    Подправил файл в package json

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ export default {
     input: 'src/index.js',
     output: [
         {
-            file: './dist/api.min.js',
+            file: './api.min.js',
             format: 'es',
             plugins: [
                 terser({
